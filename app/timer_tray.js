beec69f9582ba25c81529dfe267322bc868ccf11
John E Shepler Jr <john.sheplerjr@gmail.com>
2018-02-11 21:21:03 -0700

    decouple some code

---
+++ app/timer_tray.js
@@ -0,0 +1,32 @@
+const electon = require('electron');
+const { Tray } = electon;
+
+class TimerTray extends Tray {
+    constructor(iconPath, mainWindow){
+        super(iconPath);
+        this.mainWindow = mainWindow
+        this.on('click', this.onClick.bind(this))
+    }
+
+    onClick(event, bounds){
+        // Click event Bounds
+        const { x,y } = bounds
+        // Window Bounds
+        const { height, width } = this.mainWindow.getBounds();
+
+        if (this.mainWindow.isVisible()) {
+            this.mainWindow.hide()
+        } else{
+            const yPosition = process.platform === 'darwin' ? y : y - height ;
+            this.mainWindow.setBounds({
+                x: x - (width/2),
+                y: yPosition,
+                height,
+                width
+            })
+            this.mainWindow.show()
+        }
+    }
+}
+
+module.exports = TimerTray
